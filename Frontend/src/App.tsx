bb300db981e1ee9332d7b0d2a761c0cba605d330
Wesly Yong <z5431551@ad.unsw.edu.au>
2025-09-30 19:45:24 +1000

    Added landing page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthProvider'
 import ProtectedRoute from './components/auth/ProtectedRoute'
+import LandingPage from './components/LandingPage'
 import Login from './components/auth/Login'
 import Register from './components/auth/Register'
 import Dashboard from './components/Dashboard'
+import StartSession from './components/StartSession'
 import './App.css'
 
 function App() {
@@ -11,6 +13,7 @@ function App() {
     <AuthProvider>
       <Router>
         <Routes>
+          <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route 
@@ -21,7 +24,15 @@ function App() {
               </ProtectedRoute>
             } 
           />
-          <Route path="/" element={<Navigate to="/dashboard" replace />} />
+          <Route 
+            path="/session/new"
+            element={
+              <ProtectedRoute>
+                <StartSession />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </Router>
     </AuthProvider>
